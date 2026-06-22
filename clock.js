006f2ed897acfe74f848d7811933b96dfd6ff43f
CyberSecurityA1 <Jayanth.cyber0@gmail.com>
2026-06-22 17:18:22 +0530

    Refactored clock update logic into a function

--- clock.js
+++ clock.js
@@ -1,1 +1,20 @@
-setInterval(()=>{let e=document.getElementById('clock');if(e)e.innerText=new Date().toLocaleString()},1000);
+function updateClock(){
+
+const now = new Date();
+
+const time =
+now.toLocaleTimeString();
+
+const clock =
+document.getElementById("clock");
+
+if(clock){
+clock.innerHTML =
+"🟢 System Time: " + time;
+}
+
+}
+
+setInterval(updateClock,1000);
+
+updateClock();
